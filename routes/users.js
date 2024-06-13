d8068c4b0dda6ec8ba2a73c4ad399a88c948ac19
ZurcLeo <leonardocruz.di@gmail.com>
2024-06-13 20:59:18 +0200

    Inclusao do Facebook Login

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,15 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
+// Rota OPTIONS para lidar com requisições preflight
+router.options('*', (req, res) => {
+    res.set('Access-Control-Allow-Origin', 'https://eloscloud.com');
+    res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS');
+    res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.set('Access-Control-Max-Age', '3600');
+    res.status(204).send('');
+});
+
 router.post('/add-user', userController.addUser);
 router.get('/get-user/:id', userController.getUser);
 router.put('/update-user', userController.updateUser);
