a24a450c561c180219ec2cd24062a8d98322712c
ZurcLeo <leonardocruz.di@gmail.com>
2024-06-14 19:54:53 +0200

    Correções de CORS e captura de token

--- routes/videosdk.js
+++ routes/videosdk.js
@@ -1,20 +1,7 @@
-//routes/videosdk.js
 const express = require('express');
-const videoSdkController = require('../controllers/videoSdkController'); 
+const videoSdkController = require('../controllers/videoSdkController');
 const router = express.Router();
 const verifyToken = require('../middlewares/auth');
-
-// Adicionar cabeçalhos CORS para todas as solicitações
-router.use((req, res, next) => {
-    res.set('Access-Control-Allow-Origin', 'https://eloscloud.com');
-    res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.set('Access-Control-Allow-Credentials', 'true');
-    if (req.method === 'OPTIONS') {
-        return res.status(204).end();
-    }
-    next();
-});
 
 router.post('/start-session', verifyToken, videoSdkController.startSession);
 router.post('/end-session', verifyToken, videoSdkController.endSession);
