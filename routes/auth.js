e1f089e4ef3b40c5299297b82683d5da1d7d9e57
ZurcLeo <leonardocruz.di@gmail.com>
2024-06-14 09:12:42 +0200

    Inclusao do Facebook Login

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,17 @@
-//routes/auth.js
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-// Rota OPTIONS para lidar com requisições preflight
-router.options('*', (req, res) => {
+// Add CORS headers for all requests
+router.use((req, res, next) => {
     res.set('Access-Control-Allow-Origin', 'https://eloscloud.com');
-    res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS');
+    res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.set('Access-Control-Max-Age', '3600');
-    res.status(204).send('');
+    res.set('Access-Control-Allow-Credentials', 'true');
+    if (req.method === 'OPTIONS') {
+        return res.status(204).end();
+    }
+    next();
 });
 
 router.post('/facebook-login', authController.facebookLogin);
@@ -20,6 +22,5 @@ router.post('/logout', authController.logout);
 router.post('/login-with-provider', authController.signInWithProvider);
 router.post('/register-with-provider', authController.registerWithProvider);
 router.post('/resend-verification-email', authController.resendVerificationEmail);
-
 
 module.exports = router;
