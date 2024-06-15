63704d01f498c0080dc05cc907b01e42cc5fe5df
ZurcLeo <leonardocruz.di@gmail.com>
2024-06-15 09:10:58 +0200

    Correções de verificação de token e melhorias de CORS

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,15 +1,14 @@
-// middlewares/auth.js
 const admin = require('firebase-admin');
 
 const verifyToken = async (req, res, next) => {
-  const token = req.headers.authorization?.split(' ')[1];
+  const idToken = req.headers.authorization?.split(' ')[1];
 
-  if (!token) {
+  if (!idToken) {
     return res.status(401).json({ message: 'No token provided' });
   }
 
   try {
-    const decodedToken = await admin.auth().verifyIdToken(token);
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
     req.user = decodedToken;
     next();
   } catch (error) {
