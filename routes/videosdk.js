27e93ede005a20b662a03c14d79cf8c395f5cc63
ZurcLeo <leonardocruz.di@gmail.com>
2024-06-15 11:14:25 +0200

    Correções de verificação de token e melhorias de CORS

--- routes/videosdk.js
+++ routes/videosdk.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const videoSdkController = require('../controllers/videoSdkController');
 const router = express.Router();
+const videoSdkController = require('../controllers/videoSdkController');
 const verifyToken = require('../middlewares/auth');
 
 // Add CORS headers for all requests
