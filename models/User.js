a43129469628d0e3a642541a51533c04d9afe934
ZurcLeo <leonardocruz.di@gmail.com>
2024-07-02 01:06:55 +0200

    Atualização das conexões do backend.

--- models/User.js
+++ models/User.js
@@ -24,7 +24,7 @@ class User {
   }
 
   static async getById(id) {
-    const doc = await firestore.collection('usuarios').doc(id).get();
+    const doc = await firestore.collection('usuario').doc(id).get();
     if (!doc.exists) {
       throw new Error('Usuário não encontrado.');
     }
@@ -35,20 +35,20 @@ class User {
 
   static async create(data) {
     const user = new User(data);
-    const docRef = await firestore.collection('usuarios').add({ ...user });
+    const docRef = await firestore.collection('usuario').add({ ...user });
     user.id = docRef.id;
     return user;
   }
 
   static async update(id, data) {
-    const userRef = firestore.collection('usuarios').doc(id);
+    const userRef = firestore.collection('usuario').doc(id);
     await userRef.update(data);
     const updatedDoc = await userRef.get();
     return new User(updatedDoc.data());
   }
 
   static async delete(id) {
-    const userRef = firestore.collection('usuarios').doc(id);
+    const userRef = firestore.collection('usuario').doc(id);
     await userRef.delete();
   }
 }
