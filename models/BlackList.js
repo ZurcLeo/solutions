54da6c2b160248c622e9088be0e6f8512b34c9c5
ZurcLeo <leonardocruz.di@gmail.com>
2024-07-02 13:58:15 +0200

    Atualização das conexões do backend.

--- models/BlackList.js
+++ models/BlackList.js
@@ -9,23 +9,43 @@ const quotaguardStaticUrl = process.env.QUOTAGUARDSTATIC_URL;
 const proxyOptions = url.parse(quotaguardStaticUrl);
 const auth = proxyOptions.auth.split(':');
 
+const agent = tunnel.httpsOverHttp({
+  proxy: {
+    host: proxyOptions.hostname,
+    port: proxyOptions.port,
+    proxyAuth: `${auth[0]}:${auth[1]}`,
+  },
+});
+
 const connectDB = async () => {
-    try {
-      const client = new MongoClient(mongoUri, {
-        tls: true,
-        proxy: {
-          host: proxyOptions.hostname,
-          port: proxyOptions.port,
-          proxyAuth: `${auth[0]}:${auth[1]}`
-        }
-      });
-      await client.connect();
-      console.log('MongoDB connected...');
-    } catch (err) {
-      console.error('Error connecting to MongoDB:', err.message);
-      process.exit(1);
-    }
-  };
+  try {
+    const client = new MongoClient(mongoUri, {
+      tls: true,
+      tlsInsecure: true,
+      tlsAllowInvalidCertificates: true,
+      tlsAllowInvalidHostnames: true,
+      tlsCAFile: null,
+      tlsCertificateKeyFile: null,
+      tlsCertificateFile: null,
+      tlsCertificateFilePassword: null,
+      tlsCertificateKeyFilePassword: null,
+      tlsDisableOCSPEndpointCheck: true,
+      tlsDisableOCSP: true,
+      tlsUseSystemCA: false,
+      tlsValidate: false,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      serverSelectionTimeoutMS: 5000,
+      directConnection: true,
+      httpAgent: agent,  // Use the agent created by tunnel
+    });
+    await client.connect();
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err.message);
+    process.exit(1);
+  }
+};
 
 const blacklistSchema = new mongoose.Schema({
   token: { type: String, required: true, unique: true },
