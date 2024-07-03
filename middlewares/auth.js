7765b247dbc3bd3bf510d41fdd75fb7dfe9c12e0
ZurcLeo <leonardocruz.di@gmail.com>
2024-07-03 17:19:45 +0200

    Incluindo arquivos e logica da blacklist

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,21 +4,25 @@ const { isTokenBlacklisted } = require('../services/blacklistService');
 
 const verifyToken = async (req, res, next) => {
   const authHeader = req.headers['authorization'];
+  console.log('Authorization Header:', authHeader); // Adiciona log do cabeçalho de autorização
+  
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    console.error('No token provided or invalid format');
     return res.status(401).json({ message: 'No token provided or invalid format' });
   }
 
   const idToken = authHeader.split(' ')[1];
-  console.log('ID Token:', idToken);
+  console.log('Extracted ID Token:', idToken); // Adiciona log do token extraído
 
   try {
-    // Check if token is blacklisted
     const blacklisted = await isTokenBlacklisted(idToken);
     if (blacklisted) {
+      console.error('Token is blacklisted');
       return res.status(401).json({ message: 'Token is blacklisted' });
     }
 
     const decodedToken = await auth.verifyIdToken(idToken);
+    console.log('Decoded Token:', decodedToken); // Adiciona log do token decodificado
     req.user = decodedToken;
     return next();
   } catch (error) {
