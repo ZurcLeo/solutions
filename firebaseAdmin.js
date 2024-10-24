0ba14ac90afe0ff53ff0d806c813ec0a4fa5c552
ZurcLeo <leonardocruz.di@gmail.com>
2024-10-24 06:19:23 -0300

    Atualização do firebaseAdmin.js

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,24 +1,30 @@
-// firebaseAdmin.js
 const admin = require('firebase-admin');
+const { logger } = require('./logger');
 require('dotenv').config();
-const serviceAccount = require('./serviceAccountKey.json');
 
-// Certifique-se de ter configurado suas credenciais corretamente.
+try {
+  const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
-});
+logger.error('FIREBASE_CREDENTIALS:', serviceAccount)
 
-const db = admin.firestore();
-const auth = admin.auth();
-const storage = admin.storage().bucket();
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  });
 
-module.exports = { admin, db, auth, storage };
+  const db = admin.firestore();
+  const auth = admin.auth();
+  const storage = admin.storage().bucket();
+
+  module.exports = { admin, db, auth, storage };
+
+} catch (error) {
+  console.error("Erro ao carregar credenciais do Firebase:", error);
+}
