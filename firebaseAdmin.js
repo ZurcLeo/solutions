4790a1b63089af4429ee881328672fdd5fa41522
ZurcLeo <leonardocruz.di@gmail.com>
2024-10-25 07:33:32 -0300

    Atualizacao do BlackList e do FirebaseAdmin para que usem getFirestore

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -2,29 +2,76 @@ const admin = require('firebase-admin');
 const { logger } = require('./logger');
 require('dotenv').config();
 
-try {
-  const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
+let firebaseApp;
+let db;
+let auth;
+let storage;
 
-logger.error('FIREBASE_CREDENTIALS:', serviceAccount)
+function initializeFirebaseAdmin() {
+  try {
+    if (firebaseApp) {
+      return firebaseApp;
+    }
 
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
-  });
+    const serviceAccount = process.env.FIREBASE_CREDENTIALS;
 
-  const db = admin.firestore();
-  const auth = admin.auth();
-  const storage = admin.storage().bucket();
+    if (!serviceAccount) {
+      throw new Error('FIREBASE_CREDENTIALS não encontradas nas variáveis de ambiente');
+    }
 
-  module.exports = { admin, db, auth, storage };
+    const parsedServiceAccount = JSON.parse(serviceAccount);
 
-} catch (error) {
-  console.error("Erro ao carregar credenciais do Firebase:", error);
+    firebaseApp = admin.initializeApp({
+      credential: admin.credential.cert(parsedServiceAccount),
+      databaseURL: process.env.FIREBASE_DATABASE_URL,
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    });
+
+    db = admin.firestore();
+    console.log(db);
+    auth = admin.auth();
+    storage = admin.storage().bucket();
+
+    logger.info('Firebase Admin inicializado com sucesso', firebaseApp);
+
+    return firebaseApp;
+  } catch (error) {
+    logger.error('Erro ao inicializar Firebase Admin:', error);
+    throw error;
+  }
 }
+
+// Função para verificar token
+async function verifyToken(token) {
+  try {
+      const app = initializeFirebaseAdmin();
+      const decodedToken = await app.auth().verifyIdToken(token);
+      return decodedToken;
+  } catch (error) {
+      console.error('Erro ao verificar token:', error);
+      throw error;
+  }
+}
+
+// Getter para garantir que o `db` seja acessado corretamente
+function getFirestore() {
+  if (!db) {
+    initializeFirebaseAdmin();
+  }
+  return db;
+}
+
+module.exports = {
+  initializeFirebaseAdmin,
+  verifyToken,
+  getApp: () => firebaseApp,
+  getFirestore, // Use este getter para acessar o Firestore
+  auth,
+  storage
+};
--- models/BlackList.js
+++ models/BlackList.js
@@ -1,33 +1,119 @@
 // models/blacklist.js
-const { db, admin } = require('../firebaseAdmin');
+const { getFirestore, initializeFirebaseAdmin } = require('../firebaseAdmin');
+const { logger } = require('../logger');
+
+initializeFirebaseAdmin(); 
 
 class Blacklist {
   constructor() {
-    this.blacklistRef = db.collection('blacklist');
+    this.blacklistRef = getFirestore().collection('blacklist');
   }
 
   async addToBlacklist(token) {
-    const tokenDoc = this.blacklistRef.doc(token);
-    await tokenDoc.set({
-      createdAt: admin.firestore.FieldValue.serverTimestamp()
+    logger.info(`Iniciando a adição do token ${token} à blacklist`, {
+      service: 'blacklistService',
+      function: 'addToBlacklist',
+      token
     });
+
+    try {
+      const tokenDoc = this.blacklistRef.doc(token);
+      await tokenDoc.set({
+        createdAt: admin.firestore.FieldValue.serverTimestamp()
+      });
+
+      logger.info(`Token ${token} adicionado à blacklist com sucesso`, {
+        service: 'blacklistService',
+        function: 'addToBlacklist',
+        token
+      });
+    } catch (error) {
+      logger.error(`Erro ao adicionar o token ${token} à blacklist`, {
+        service: 'blacklistService',
+        function: 'addToBlacklist',
+        token,
+        error: error.message
+      });
+
+      throw new Error('Erro ao adicionar token à blacklist.');
+    }
   }
 
   async isTokenBlacklisted(token) {
-    const tokenDoc = await this.blacklistRef.doc(token).get();
-    return tokenDoc.exists;
+    logger.info(`Verificando se o token ${token} está na blacklist`, {
+      service: 'blacklistService',
+      function: 'isTokenBlacklisted',
+      token
+    });
+
+    try {
+      const tokenDoc = await this.blacklistRef.doc(token).get();
+      
+      if (tokenDoc.exists) {
+        logger.info(`Token ${token} está na blacklist`, {
+          service: 'blacklistService',
+          function: 'isTokenBlacklisted',
+          token
+        });
+      } else {
+        logger.warn(`Token ${token} não está na blacklist`, {
+          service: 'blacklistService',
+          function: 'isTokenBlacklisted',
+          token
+        });
+      }
+
+      return tokenDoc.exists;
+    } catch (error) {
+      logger.error(`Erro ao verificar se o token ${token} está na blacklist`, {
+        service: 'blacklistService',
+        function: 'isTokenBlacklisted',
+        token,
+        error: error.message
+      });
+
+      throw new Error('Erro ao verificar token na blacklist.');
+    }
   }
 
   async removeExpiredTokens() {
-    const now = admin.firestore.Timestamp.now();
-    const snapshot = await this.blacklistRef.where('createdAt', '<', now.toDate()).get();
-    
-    const batch = db.batch();
-    snapshot.forEach((doc) => {
-      batch.delete(doc.ref);
+    logger.info('Iniciando a remoção de tokens expirados da blacklist', {
+      service: 'blacklistService',
+      function: 'removeExpiredTokens'
     });
-    
-    await batch.commit();
+
+    try {
+      const now = admin.firestore.Timestamp.now();
+      const snapshot = await this.blacklistRef.where('createdAt', '<', now.toDate()).get();
+      
+      if (snapshot.empty) {
+        logger.info('Nenhum token expirado encontrado para remoção', {
+          service: 'blacklistService',
+          function: 'removeExpiredTokens'
+        });
+        return;
+      }
+
+      const batch = getFirestore().batch();
+      snapshot.forEach((doc) => {
+        batch.delete(doc.ref);
+      });
+      
+      await batch.commit();
+
+      logger.info('Tokens expirados removidos com sucesso', {
+        service: 'blacklistService',
+        function: 'removeExpiredTokens'
+      });
+    } catch (error) {
+      logger.error('Erro ao remover tokens expirados da blacklist', {
+        service: 'blacklistService',
+        function: 'removeExpiredTokens',
+        error: error.message
+      });
+
+      throw new Error('Erro ao remover tokens expirados.');
+    }
   }
 }
 
